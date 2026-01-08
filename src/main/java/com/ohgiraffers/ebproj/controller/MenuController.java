570797e841e2596fface6d4e4f71b54e249b6eba
gongyoonkyung <rorakong03@gmail.com>
2026-01-08 13:49:24 +0900

    중간 수정

--- src/main/java/com/ohgiraffers/ebproj/controller/MenuController.java
+++ src/main/java/com/ohgiraffers/ebproj/controller/MenuController.java
@@ -9,4 +9,9 @@ public class MenuController {
     public String healthCheck(){
         return  "I'm OK";
     }
+
+    @GetMapping("/menu")
+    public  String menu(){
+        return  "I'm OK";
+    }
 }
